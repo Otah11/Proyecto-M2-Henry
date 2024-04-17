f328b5e469ceba31aaa3890289caba518bcabead
otah11 <juanalbertoaraujo@gmail.com>
2024-04-16 22:23:00 -0300

    weas

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -3,7 +3,7 @@ import { renderCards } from './render_cards.js';
 
 const moviesContainer = document.getElementById("movies-container");
 
-// Usar Axios en lugar de fetchMovies
+
 axios.get("https://webpt19b.web.app/data/movies.json")
   .then(response => renderCards(response.data, moviesContainer))
   .catch(() => alert("Error al obtener los datos de las películas."));
